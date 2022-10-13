54a91f28e4d2ea5a641eb8c3da7cb48e5c681e6f
Christian Areas <me@areas.me>
2022-10-13 16:06:37 -0700

    Added start page and notes for the script.

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// - Add a start page with a “Start Quiz” button.
+// - When the user clicks the “Start Quiz” button, start a countdown timer.
+// - When the user starts the quiz, present a series of multiple-answer questions.
+// - As the user answers questions, evaluate whether the answer is correct or incorrect.
+// - As the user answers questions, store their answers in local storage (maybe).
+// - At the end of the quiz, present the user with their total score.
+// - At the end of the quiz, ask the user for their initials.
+// - When the user submits their score, save it to local storage and display it on a high scores page.
+// - Let the user clear the high scores.
+// - If the user answer incorrectly, subtract time from the timer.
+// - When the user finishes the quiz or time runs out, end the quiz.
+// - Bonus: Use Bootstrap for UI.
+
